3179db8a4e97dd60fb2925a9f32e0a49c79cfc89
PJ Katema <panashe.katema@gmail.com>
2022-07-06 22:28:52 +0100

    fixed import statemnts

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes,  Route} from "react-router-dom";
+import Home from './views/Home';
+import NotFound from './views/NotFound';
 
 function App() {
   return (
     <div className="shane_tm_all_wrap">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
